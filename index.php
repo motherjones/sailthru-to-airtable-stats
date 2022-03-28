3fafed9f58c8aff7b5f08ffe24aa36c47f2508b8
Young Kim <ykim@motherjones.com>
2022-03-28 12:03:45 -0700

    Creating new repo
    This is for the Sailthru to Airtable stats Heroku app.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$traceback = $_SERVER['HTTP_REFERER'];
+
+echo $traceback;
+
+/*if(stripos($traceback, "airtableblocks.com") === false) {
+    console.log($traceback);
+    $err_msg["message"] = "Not allowed";
+    echo json_encode($err_msg);
+    die();
+}*/
+?>
