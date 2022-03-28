5c434284b9da7d9dc59ae56ed3873f3c13fb5c74
Young Kim <ykim@motherjones.com>
2022-03-28 14:20:56 -0700

    ready for cross browser calls

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$traceback = $_SERVER['HTTP_REFERER'];
+$traceback["url"] = $_SERVER['HTTP_REFERER'];
 
-echo $traceback;
+/*if(stripos($traceback, "airtableblocks.com") === false) {
+    console.log($traceback);
+    $err_msg["message"] = "Not allowed";
+    echo json_encode($err_msg);
+    die();
+}*/
+
+echo json_encode($traceback["url"]);
  ?>
