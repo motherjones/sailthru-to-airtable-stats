13be8bbe1691ba3e451b78df9c1420d7d522ccb0
Young Kim <ykim@motherjones.com>
2022-03-28 13:44:38 -0700

    I don't know why this isn't working ;

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'eef7b656168b1134ee58165d7c3e4b19f8dca08f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'eef7b656168b1134ee58165d7c3e4b19f8dca08f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
