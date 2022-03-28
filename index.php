eef7b656168b1134ee58165d7c3e4b19f8dca08f
Young Kim <ykim@motherjones.com>
2022-03-28 13:16:43 -0700

    hope this works

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
+
+echo "this is an app";
+
+/*
 header("Access-Control-Allow-Origin: *");
 
 $traceback = $_SERVER['HTTP_REFERER'];
 
 echo $traceback;
 
-/*if(stripos($traceback, "airtableblocks.com") === false) {
+if(stripos($traceback, "airtableblocks.com") === false) {
     console.log($traceback);
     $err_msg["message"] = "Not allowed";
     echo json_encode($err_msg);
