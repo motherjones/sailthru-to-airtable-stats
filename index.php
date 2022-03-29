0bb39aa974e054944b2268e578ba1d9cc503490f
Young Kim <ykim@motherjones.com>
2022-03-28 17:12:57 -0700

    almost making the call

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$traceback["url"] = $_SERVER['HTTP_REFERER'];
-
+$traceback = $_SERVER['HTTP_REFERER'];
 /*if(stripos($traceback, "airtableblocks.com") === false) {
     console.log($traceback);
     $err_msg["message"] = "Not allowed";
@@ -10,5 +9,19 @@ $traceback["url"] = $_SERVER['HTTP_REFERER'];
     die();
 }*/
 
-echo json_encode($traceback["url"]);
+//example call below
+//curl 'https://api.sailthru.com/<endpoint>?api_key=<key>&sig=<sig>&format=<json_or_xml>&json=<url_escaped_data>'
+//the stats url
+//https://api.sailthru.com/stats?api_key=$api_key&sig=$sig&json=
+
+$secret = getenv("secret");
+$api_key = getenv("api_key");
+$type = "json";
+$payload = json_encode({"stat":"list"});
+//get the sig
+$sig = md5($secret . $api_key . $type . $payload);
+
+$return_me["return"] = $sig;
+
+echo json_encode($return_me);
  ?>
